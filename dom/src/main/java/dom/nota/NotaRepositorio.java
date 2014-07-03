25a1f2655de02929845877f9108d10d14a11547d
munozdaniel <cipoleto@gmail.com>
2014-07-03 12:06:20 -0300

    Nota: Se mejoro el metodo insertar

--- dom/src/main/java/dom/nota/NotaRepositorio.java
+++ dom/src/main/java/dom/nota/NotaRepositorio.java
@@ -1,12 +1,22 @@
 package dom.nota;
 
+import java.util.Formatter;
+import java.util.List;
+
 import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.DescribedAs;
 import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.MinLength;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Optional;
+import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.RegEx;
+import org.apache.isis.applib.query.QueryDefault;
 import org.joda.time.LocalDate;
 
-import dom.sectores.Sector;
+import dom.sector.Sector;
+import dom.sector.SectorRepositorio;
+import dom.tecnico.Tecnico;
 
 @Named("Notas")
 public class NotaRepositorio {
@@ -27,55 +37,70 @@ public class NotaRepositorio {
 		return "Tecnico";
 	}
 
-	// //////////////////////////////////////
-	// Insertar un Sector.
-	// //////////////////////////////////////
-	@Named("Agregar")
+	/**
+	 * addNota
+	 * 
+	 * @param sector
+	 * @param destino
+	 * @param descripcion
+	 * @return
+	 */
+	@Named("Enviar")
 	@MemberOrder(sequence = "10")
 	public Nota addNota(
-			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("De:") Sector sector,
+			final @Optional @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("De:") Sector sector,
 			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Para:") String destino,
-			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Descripción:") String descripcion			
-			) {
-//		return nuevaNota(sector, destino, descripcion);
-		return nuevaNota(sector,destino,descripcion,this.currentUserName());
+			final @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("Descripción:") String descripcion) {
+		// return nuevaNota(sector, destino, descripcion);
+		return nuevaNota(sector, destino, descripcion, this.currentUserName());
 	}
 
-	private Nota nuevaNota(final Sector sector,final String destino,
+	@Programmatic
+	private Nota nuevaNota(final Sector sector, final String destino,
 			final String descripcion, final String currentUserName) {
 		final Nota unaNota = this.container.newTransientInstance(Nota.class);
-		unaNota.setNro_nota(1);
+		int nro = recuperarNroNota();
+		nro += 1;
+		formato = new Formatter();
+		formato.format("%04d", nro);
+		unaNota.setNro_nota(Integer.parseInt(formato.toString()));
 		unaNota.setFecha(LocalDate.now());
 		unaNota.setTipo(1);
-		unaNota.setDescripcion(descripcion);
+		unaNota.setDescripcion(descripcion.toUpperCase().trim());
 		unaNota.setHabilitado(true);
 		container.persistIfNotAlready(unaNota);
 		container.flush();
 		return unaNota;
 	}
 
-	/**
-	 * Buscar
-	 * 
-	 * @param nombreSector
-	 * @return
-	 */
+	@Programmatic
+	private int recuperarNroNota() {
+		final Integer nroNota = this.container
+				.uniqueMatch(new QueryDefault<Integer>(Integer.class,
+						"buscarUltimaNotaTrue"));
+		if (nroNota == null || nroNota == 0)
+			return 0;
+		else
+			return nroNota;
+	}
 
+	// //////////////////////////////////////
+	// Buscar Tecnico
+	// //////////////////////////////////////
 
-	// @Programmatic
-	// public Nota nuevaNota(final Sector sector, final String destino,
-	// final String descripcion) {
-	// final Nota unaNota = this.container
-	// .newTransientInstance(Nota.class);
-	//
-	// unSector.setNombreSector(nombreSector.toUpperCase().trim());
-	// unSector.setHabilitado(true);
-	// unSector.setCreadoPor(creadoPor);
-	// this.container.persistIfNotAlready(unSector);
-	// this.container.flush();
-	// return unSector;
-	//
-	// }
+	@Named("Sector")
+	@DescribedAs("Buscar el Sector en mayuscula")
+	public List<Sector> autoComplete0AddNota(
+			final @MinLength(2) String search) {
+		return sectorRepositorio.autoComplete(search);
+	}
+
+	@Programmatic
+	public List<Nota> autoComplete(final String destino) {
+		return container.allMatches(new QueryDefault<Nota>(Nota.class,
+				"autoCompletarDestino",
+				"destino",destino));
+	}
 
 	// //////////////////////////////////////
 	// CurrentUserName
@@ -91,4 +116,7 @@ public class NotaRepositorio {
 
 	@javax.inject.Inject
 	private DomainObjectContainer container;
+	@javax.inject.Inject
+	private SectorRepositorio sectorRepositorio;
+	private Formatter formato;
 }
