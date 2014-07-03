a0347a35770b578b12d2e654982fddd81776cd5f
munozdaniel <cipoleto@gmail.com>
2014-07-03 09:53:46 -0300

    Cambiado Atributos en Documento

--- dom/src/main/java/dom/documento/Documento.java
+++ dom/src/main/java/dom/documento/Documento.java
@@ -9,36 +9,40 @@ import org.apache.isis.applib.annotation.Hidden;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Where;
 import org.apache.isis.applib.util.ObjectContracts;
+import org.joda.time.LocalDate;
 
 @PersistenceCapable
 @Inheritance(strategy = InheritanceStrategy.SUBCLASS_TABLE)
 public abstract class Documento implements Comparable<Documento> {
 	// pk private id_documento;
 
-//	private int nro_documento;
-//
-//	@javax.jdo.annotations.Column(allowsNull = "false")
-//	@MemberOrder(sequence = "10")
-//	public int getNro_documento() {
-//		return nro_documento;
-//	}
-//
-//	public void setNro_documento(int nro_documento) {
-//		this.nro_documento = nro_documento;
-//	}
+	// private int nro_documento;
+	//
+	// @javax.jdo.annotations.Column(allowsNull = "false")
+	// @MemberOrder(sequence = "10")
+	// public int getNro_documento() {
+	// return nro_documento;
+	// }
+	//
+	// public void setNro_documento(int nro_documento) {
+	// this.nro_documento = nro_documento;
+	// }
 
-	private int fecha;
+	private LocalDate fecha;
 
 	@javax.jdo.annotations.Column(allowsNull = "false")
 	@MemberOrder(sequence = "20")
-	public int getFecha() {
+	public LocalDate getFecha() {
 		return fecha;
 	}
 
-	public void setFecha(int fecha) {
+	public void setFecha(LocalDate fecha) {
 		this.fecha = fecha;
 	}
 
+	/*
+	 * tipo - 1:Nota - 2:Memo - 3:Resoluciones - 4:Disposiciones - 5:Expedientes
+	 */
 	private int tipo;
 
 	@javax.jdo.annotations.Column(allowsNull = "false")
@@ -63,41 +67,43 @@ public abstract class Documento implements Comparable<Documento> {
 		this.descripcion = descripcion;
 	}
 
-	private int eliminado;
+	private Boolean habilitado;
 
 	@javax.jdo.annotations.Column(allowsNull = "false")
 	@MemberOrder(sequence = "60")
-	public int getEliminado() {
-		return eliminado;
+	public Boolean getHabilitado() {
+		return habilitado;
 	}
 
-	public void setEliminado(int eliminado) {
-		this.eliminado = eliminado;
+	public void setHabilitado(Boolean habilitado) {
+		this.habilitado = habilitado;
 	}
 
-	private int ultimo;
-
-	@javax.jdo.annotations.Column(allowsNull = "false")
-	@MemberOrder(sequence = "70")
-	public int getUltimo() {
-		return ultimo;
-	}
-
-	public void setUltimo(int ultimo) {
-		this.ultimo = ultimo;
-	}
-
-	private String observacion;
-
-	@javax.jdo.annotations.Column(allowsNull = "false")
-	@MemberOrder(sequence = "80")
-	public String getObservacion() {
-		return observacion;
-	}
-
-	public void setObservacion(String observacion) {
-		this.observacion = observacion;
-	}
+	// ELIMINADO se obtendra el ultimo a travez de una consulta, con el MAX, si
+	// es posible.
+	// private int ultimo;
+	//
+	// @javax.jdo.annotations.Column(allowsNull = "false")
+	// @MemberOrder(sequence = "70")
+	// public int getUltimo() {
+	// return ultimo;
+	// }
+	//
+	// public void setUltimo(int ultimo) {
+	// this.ultimo = ultimo;
+	// }
+	// Observacion; todos los campos en la base de datos se encontraban null.
+	// private String observacion;
+	//
+	// @javax.jdo.annotations.Column(allowsNull = "false")
+	// @MemberOrder(sequence = "80")
+	// public String getObservacion() {
+	// return observacion;
+	// }
+	//
+	// public void setObservacion(String observacion) {
+	// this.observacion = observacion;
+	// }
 
 	// //////////////////////////////////////
 	// creadoPor
@@ -116,39 +122,54 @@ public abstract class Documento implements Comparable<Documento> {
 	}
 
 	// //////////////////////////////////////
+	// DueBy (property)
+	// //////////////////////////////////////
+
+	@javax.jdo.annotations.Persistent(defaultFetchGroup = "true")
+	private LocalDate dueBy;
+
+	@javax.jdo.annotations.Column(allowsNull = "true")
+	public LocalDate getDueBy() {
+		return dueBy;
+	}
+
+	public void setDueBy(final LocalDate dueBy) {
+		this.dueBy = dueBy;
+	}
+
+	// //////////////////////////////////////
 	// RELACION: Documento - Nota
 	// //////////////////////////////////////
 
 	// {{ Nota (property)
-//	private Nota nota;
-//
-//	@MemberOrder(sequence = "1")
-//	@Column(allowsNull = "False")
-//	public Nota getNota() {
-//		return nota;
-//	}
-//
-//	public void setNota(final Nota nota) {
-//		this.nota = nota;
-//	}
+	// private Nota nota;
+	//
+	// @MemberOrder(sequence = "1")
+	// @Column(allowsNull = "False")
+	// public Nota getNota() {
+	// return nota;
+	// }
+	//
+	// public void setNota(final Nota nota) {
+	// this.nota = nota;
+	// }
 	// }}
-
 
 	// //////////////////////////////////////
 	// RELACIONES: Documento - Sector
 	// //////////////////////////////////////
 
-//	private int id_sector;
-//
-//	@javax.jdo.annotations.Column(allowsNull = "false")
-//	@MemberOrder(sequence = "40")
-//	public int getId_sector() {
-//		return id_sector;
-//	}
-//
-//	public void setId_sector(int id_sector) {
-//		this.id_sector = id_sector;
-//	}
+	// private int id_sector;
+	//
+	// @javax.jdo.annotations.Column(allowsNull = "false")
+	// @MemberOrder(sequence = "40")
+	// public int getId_sector() {
+	// return id_sector;
+	// }
+	//
+	// public void setId_sector(int id_sector) {
+	// this.id_sector = id_sector;
+	// }
 
 	// //////////////////////////////////////
 	// Implementando los metodos de comparable
