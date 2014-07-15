a327ac9cd1d768f67572824cdc6bdff8638f9a3b
munozdaniel <cipoleto@gmail.com>
2014-07-15 14:02:42 -0300

    ResolucionesRepo: Agregado met. para filtrar.

--- dom/src/main/java/dom/resoluciones/ResolucionesRepositorio.java
+++ dom/src/main/java/dom/resoluciones/ResolucionesRepositorio.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Optional;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.RegEx;
 import org.apache.isis.applib.query.QueryDefault;
@@ -90,6 +91,62 @@ public class ResolucionesRepositorio {
 		}
 		return listaMemo;
 
+	}
+
+	// //////////////////////////////////////
+	// Filtrar por Fecha o Sector
+	// //////////////////////////////////////
+
+	@MemberOrder(sequence = "30")
+	public List<Resoluciones> filtrar(
+			final @Optional @RegEx(validation = "[a-zA-Záéíóú]{2,15}(\\s[a-zA-Záéíóú]{2,15})*") @Named("De:") Sector sector,
+			final @Optional @Named("Fecha") LocalDate fecha) {
+		if (fecha == null && sector == null) {
+			this.container.warnUser("Sin Filtro");
+			return this.listar();
+
+		} else {
+			if (fecha != null && sector == null) {
+				final List<Resoluciones> filtrarPorFecha = this.container
+						.allMatches(new QueryDefault<Resoluciones>(
+								Resoluciones.class, "filtrarPorFecha", "fecha",
+								fecha));
+
+				if (filtrarPorFecha.isEmpty()) {
+					this.container.warnUser("No se encontraron Notas.");
+				}
+				this.container.warnUser("Filtrado por Fechas.");
+
+				return filtrarPorFecha;
+			} else if (fecha == null && sector != null) {
+				final List<Resoluciones> filtrarPorSector = this.container
+						.allMatches(new QueryDefault<Resoluciones>(
+								Resoluciones.class, "filtrarPorSector",
+								"sector", sector));
+				this.container.warnUser("Filtrado por Sector.");
+
+				if (filtrarPorSector.isEmpty()) {
+					this.container.warnUser("No se encontraron Notas.");
+				}
+				return filtrarPorSector;
+			} else {
+				final List<Resoluciones> filtrarFechaSector = this.container
+						.allMatches(new QueryDefault<Resoluciones>(
+								Resoluciones.class, "filtrarPorFechaSector",
+								"fecha", fecha, "sector", sector));
+				this.container.warnUser("Filtrado por Fecha y Sector.");
+
+				if (filtrarFechaSector.isEmpty()) {
+					this.container.warnUser("No se encontraron Notas.");
+				}
+				return filtrarFechaSector;
+			}
+		}
+	}
+
+	@Named("Sector")
+	public List<Sector> choices0Filtrar() {
+		return sectorRepositorio.listarResoluciones();
 	}
 
 	// //////////////////////////////////////
