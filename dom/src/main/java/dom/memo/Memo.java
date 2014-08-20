76c2ad4bd1e6b695c90ebf4b3d5a845f77fcfb0a
munozdaniel <cipoleto@gmail.com>
2014-08-20 14:49:09 -0300

    Memo: Restaurar memo.

--- dom/src/main/java/dom/memo/Memo.java
+++ dom/src/main/java/dom/memo/Memo.java
@@ -169,6 +169,21 @@ public class Memo extends Documento {
 			return true;
 	}
 
+	@Named("Restaurar")
+	@DescribedAs("Necesario privilegios de Administrador.")
+	public Memo restaurar() {
+		this.setHabilitado(true);
+		return this;
+	}
+
+	public boolean hideRestaurar() {
+		// TODO: return true if action is hidden, false if
+		// visible
+		if (this.container.getUser().isCurrentUser("root"))
+			return false;
+		else
+			return true;
+	}
 
 	@javax.inject.Inject
 	private MemoRepositorio memoRepositorio;
