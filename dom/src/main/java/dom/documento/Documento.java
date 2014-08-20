ff9132bbc33c11f39f3c2753082eaadf24e0fb0a
munozdaniel <cipoleto@gmail.com>
2014-08-20 12:05:42 -0300

    Documento: Agregado choicesSector

--- dom/src/main/java/dom/documento/Documento.java
+++ dom/src/main/java/dom/documento/Documento.java
@@ -1,5 +1,7 @@
 package dom.documento;
 
+import java.util.List;
+
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.Inheritance;
 import javax.jdo.annotations.InheritanceStrategy;
@@ -13,13 +15,13 @@ import org.apache.isis.applib.annotation.MaxLength;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.MultiLine;
 import org.apache.isis.applib.annotation.Named;
-import org.apache.isis.applib.annotation.Where;
 import org.apache.isis.applib.util.ObjectContracts;
 import org.apache.isis.applib.value.Blob;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 
 import dom.sector.Sector;
+import dom.sector.SectorRepositorio;
 
 @PersistenceCapable
 @Inheritance(strategy = InheritanceStrategy.SUBCLASS_TABLE)
@@ -43,7 +45,6 @@ public abstract class Documento implements Comparable<Documento> {
 
 	@Disabled
 	@javax.jdo.annotations.Column(allowsNull = "false")
-	// @MemberOrder(name="Datos Generales" ,sequence = "20")
 	@Named("Fecha")
 	public LocalDate getFecha() {
 		return fecha;
@@ -99,40 +100,11 @@ public abstract class Documento implements Comparable<Documento> {
 		this.habilitado = habilitado;
 	}
 
-	// ELIMINADO se obtendra el ultimo a travez de una consulta, con el MAX, si
-	// es posible.
-	// private int ultimo;
-	//
-	// @javax.jdo.annotations.Column(allowsNull = "false")
-	// @MemberOrder(sequence = "70")
-	// public int getUltimo() {
-	// return ultimo;
-	// }
-	//
-	// public void setUltimo(int ultimo) {
-	// this.ultimo = ultimo;
-	// }
-	// Observacion; todos los campos en la base de datos se encontraban null.
-	// private String observacion;
-	//
-	// @javax.jdo.annotations.Column(allowsNull = "false")
-	// @MemberOrder(sequence = "80")
-	// public String getObservacion() {
-	// return observacion;
-	// }
-	//
-	// public void setObservacion(String observacion) {
-	// this.observacion = observacion;
-	// }
-	// //////////////////////////////////////
-	// Attachment (property)
-	// //////////////////////////////////////
-
 	private Blob adjuntar;
-	
+
 	@MemberOrder(name = "Observaciones", sequence = "50")
 	@javax.jdo.annotations.Persistent(defaultFetchGroup = "false")
-	@javax.jdo.annotations.Column(allowsNull = "true",name="adjunto")
+	@javax.jdo.annotations.Column(allowsNull = "true", name = "adjunto")
 	@Named("Adjuntar")
 	public Blob getAdjuntar() {
 		return adjuntar;
@@ -164,6 +136,7 @@ public abstract class Documento implements Comparable<Documento> {
 	// @MemberOrder(name="Datos Generales" ,sequence = "30")
 	@Column(allowsNull = "False")
 	@Named("Origen")
+	// @Hidden(where=Where.STANDALONE_TABLES)
 	public Sector getSector() {
 		return sector;
 	}
@@ -177,6 +150,12 @@ public abstract class Documento implements Comparable<Documento> {
 			this.setSector(null);
 	}
 
+	public List<Sector> choicesSector() {
+		return this.sectorRepositorio.listar();
+	}
+
+	@javax.inject.Inject
+	private SectorRepositorio sectorRepositorio;
 	// }}
 	// {{ Ultimo (property)
 	private Boolean ultimo;
@@ -192,28 +171,13 @@ public abstract class Documento implements Comparable<Documento> {
 		this.ultimo = ultimo;
 	}
 
-	// }}
-	// {{ EsAnioNuevo (property)
-	// private Boolean esAnioNuevo;
-	//
-	// @MemberOrder(sequence = "1")
-	// @javax.jdo.annotations.Column(allowsNull = "true")
-	// @Hidden
-	// @Disabled
-	// public Boolean getEsAnioNuevo() {
-	// return esAnioNuevo;
-	// }
-	//
-	// public void setEsAnioNuevo(final Boolean esAnioNuevo) {
-	// this.esAnioNuevo = esAnioNuevo;
-	// }
-	// }}
-
-
+	// //////////////////////////////////////
+	// CompareTo
+	// //////////////////////////////////////
 
 	@Override
 	public int compareTo(Documento documento) {
-		return ObjectContracts.compare(this, documento, "time");
+		return ObjectContracts.compare(this, documento, "time,descripcion");
 	}
 
 	// //////////////////////////////////////
