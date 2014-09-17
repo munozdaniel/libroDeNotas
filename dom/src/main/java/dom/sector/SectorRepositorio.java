774e14d40b39779d5d499822160317c6096c7584
munozdaniel <cipoleto@gmail.com>
2014-09-17 11:47:17 -0300

    @NotContributed a metodo buscar de sector.

--- dom/src/main/java/dom/sector/SectorRepositorio.java
+++ dom/src/main/java/dom/sector/SectorRepositorio.java
@@ -7,6 +7,7 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.MinLength;
 import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.NotContributed;
 import org.apache.isis.applib.annotation.Optional;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.query.QueryDefault;
@@ -95,7 +96,7 @@ public class SectorRepositorio {
 	 * @param nombreSector
 	 * @return
 	 */
-
+	@NotContributed
 	@MemberOrder(sequence = "21")
 	public List<Sector> buscar(
 			final @Named("Nombre") @MinLength(2) String nombreSector) {
