b034d67398d8236bbf7a1d2ede7dd55cf14e036f
munozdaniel <cipoleto@gmail.com>
2014-11-07 08:13:30 -0300

    renombrando paquete dashboard a inicio.

--- dom/src/main/java/dom/inicio/NotaDashboard.java
+++ dom/src/main/java/dom/inicio/NotaDashboard.java
@@ -1,4 +1,4 @@
-package dom.dashboard;
+package dom.inicio;
 
 import java.util.List;
 
--- dom/src/main/java/dom/inicio/NotaDashboardService.java
+++ dom/src/main/java/dom/inicio/NotaDashboardService.java
@@ -1,4 +1,4 @@
-package dom.dashboard;
+package dom.inicio;
 
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.ActionSemantics;
