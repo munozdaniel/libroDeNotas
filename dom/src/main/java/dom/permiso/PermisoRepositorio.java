65329363c6be93dcc2882d4b7e26101c2c5f3f98
munozdaniel <cipoleto@gmail.com>
2014-11-10 11:45:09 -0300

    PermisoRepo: Agregado package predeterminados

--- dom/src/main/java/dom/permiso/PermisoRepositorio.java
+++ dom/src/main/java/dom/permiso/PermisoRepositorio.java
@@ -21,7 +21,10 @@
  */
 package dom.permiso;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.isis.applib.DomainObjectContainer;
 import org.apache.isis.applib.annotation.ActionSemantics;
@@ -31,6 +34,7 @@ import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.MemberOrder;
 import org.apache.isis.applib.annotation.Named;
 import org.apache.isis.applib.annotation.Optional;
+import org.reflections.Reflections;
 
 @DomainService(menuOrder = "82", repositoryFor = Permiso.class)
 @Named("Permisos")
@@ -44,15 +48,24 @@ public class PermisoRepositorio {
 		return "Tecnico";
 	}
 
-	@MemberOrder(sequence = "2")
+	@MemberOrder(sequence = "1")
 	@Named("Nuevo Permiso")
 	public Permiso addPermiso(
 			final @Named("Nombre") String nombre,
-			final @Named("Directorio") String path,
-			@Optional @DescribedAs("Por defecto: '*' ") @Named("Clase") String clase,
-			@Optional @DescribedAs("Por defecto: '*' ") @Named("Metodo/Atributo") String campo,
-			final @Optional @DescribedAs("Por defecto: lectura/escritura ") @Named("Permiso de Escritura") boolean escritura) {
+			final @Optional @Named("Package") String pack,
+			final @Named("Otro package?") boolean nuevo,
+			final @Optional @Named("Nuevo Package") String nuevoPackage,
+			@Optional @DescribedAs("Todas las clases: '*' ") @Named("Clase") String clase,
+			@Optional @DescribedAs("Todos los Met/Atr: '*' ") @Named("Metodo/Atributo") String campo,
+			final @Optional @Named("Permiso de Escritura") boolean escritura) {
+
 		final Permiso permiso = container.newTransientInstance(Permiso.class);
+
+		String paquete = "";
+		if (pack == null)
+			paquete = nuevoPackage;
+		else
+			paquete = pack;
 
 		permiso.setNombre(nombre.toUpperCase().trim());
 		if (clase == "" || clase == null)
@@ -62,23 +75,67 @@ public class PermisoRepositorio {
 		String acceso = "*";
 		if (!escritura)
 			acceso = "r";
-		String directorio = path + ":" + clase + ":" + campo + ":" + acceso;
+		String directorio = paquete + ":" + clase + ":" + campo + ":" + acceso;
 
 		permiso.setPath(directorio);
-
+		permiso.setNombre(nombre);
 		container.persistIfNotAlready(permiso);
 		return permiso;
 	}
 
-	public String default2AddPermiso() {
+	public String validateAddPermiso(final String nombre, final String pack,
+			final boolean nuevo, final String nuevoPackage, String clase,
+			String campo, boolean escritura) {
+		if (nuevo && (nuevoPackage == null || nuevoPackage == ""))
+			return "Ingrese un nuevo Package.";
+		if (!nuevo && (pack == null || pack == ""))
+			return "Seleccione un Package.";
+		return null;
+	}
+
+	public List<String> choices1AddPermiso(String nombre, String pack,
+			boolean nuevo) {
+		if (nuevo)
+			return null;
+		else {
+			List<String> lista = new ArrayList<String>();
+			for (Package p : Package.values()) {
+				lista.add(p.toString());
+			}
+			return lista;
+		}
+	}
+
+	public String default4AddPermiso() {
 		return "*";
 	}
 
-	public String default3AddPermiso() {
+	// public List<String> choices4AddPermiso(String nombre, String pack,
+	// boolean nuevo, String nuevoPackage, String clase)
+	// throws ClassNotFoundException {
+	// String path;
+	// if (nuevo)
+	// path = nuevoPackage;
+	// else
+	// path = pack;
+	// Class userClass = Class.forName("dom.rol.Rol");
+	//if(esCampo)//Agregar campo booleano esCampo
+	//  Field[] method = aClass.getFields();
+	//else
+	// Method[] method = userClass.getMethods();
+	// List<String> retorno = new ArrayList<String>();
+	//retorno.add("*");
+	// for (int i = 0; i < method.length; i++) {
+	// retorno.add(method[i].getName());
+	// }
+	// return retorno;
+	// }
+
+	public String default5AddPermiso() {
 		return "*";
 	}
 
-	public boolean default4AddPermiso() {
+	public boolean default6AddPermiso() {
 		return true;
 	}
 
@@ -93,17 +150,58 @@ public class PermisoRepositorio {
 	}
 
 	@ActionSemantics(Of.SAFE)
-	@MemberOrder(sequence = "1")
+	@MemberOrder(sequence = "2")
 	@Named("Todos los Permisos")
 	public List<Permiso> listAll() {
 		return container.allInstances(Permiso.class);
 	}
 
-	public static enum CategoriaClase {
-		Seagate, Western, Otro;
+	public enum Package {
+		DISPOSICIONES("dom.disposicion"), DOCUMENTOS("dom.documento"), EXPEDIENTES(
+				"dom.expediente"), INICIO("dom.inicio"), MEMO("dom.memo"), NOTA(
+				"dom.nota"), PERMISO("dom.permiso"), RESOLUCIONES(
+				"dom.resoluciones"), ROL("dom.rol"), SECTOR("dom.sector"), USUARIO(
+				"dom.usuario"), SERVICES("dom.services"), ;
+
+		private final String text;
+
+		/**
+		 * @param text
+		 */
+		private Package(final String text) {
+			this.text = text;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Enum#toString()
+		 */
+		@Override
+		public String toString() {
+			return text;
+		}
 	}
 
+	public void prueba(final @Named("package") Package pack,
+			final @Named("Clases Reflection") String clases) {
 
+	}
+
+	public List<String> choices1Prueba(Package pack, String clases) {
+		Reflections reflections = new Reflections("dom.rol");
+
+		Set<Class<? extends Object>> allClasses = reflections
+				.getSubTypesOf(Object.class);
+
+		System.out.println("CLase " + allClasses.size());
+		List<String> lista = new ArrayList<String>();
+		for (Iterator<Class<? extends Object>> it = allClasses.iterator(); it
+				.hasNext();) {
+			lista.add(it.next().toString());
+		}
+		return lista;
+	}
 
 	@javax.inject.Inject
 	DomainObjectContainer container;
